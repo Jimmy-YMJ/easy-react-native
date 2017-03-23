049b02f205994eb069d606ae2690c4acad31d681
Jimmy-YMJ <jimmy@symfx.com>
2017-03-23 17:59:01 +0800

    Add deep-equal to reduce component render rate

--- src/Page.js
+++ src/Page.js
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react';
+import deepEqual from 'deep-equal';
 import {
   View
 } from 'react-native';
 
 class Page extends Component{
   shouldComponentUpdate(nextProps){
-    return nextProps._pageHide !== true || (nextProps._pageHide === true && (this.props._pageHide !== nextProps._pageHide));
+    return (nextProps._pageHide !== true && deepEqual(nextProps, this.props)) || (nextProps._pageHide === true && (this.props._pageHide !== nextProps._pageHide));
   }
   render(){
     return <View {...this.props}>{this.props.children}</View>;
